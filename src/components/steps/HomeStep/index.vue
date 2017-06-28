c8e07f4d4dff4e585926d2f84da1ac6327ab9b79
Walker Kennedy <walkerkennedy@Walkers-MacBook-Pro.local>
2017-06-27 20:57:01 -0400

    Initial setup for home page

---
+++ src/components/steps/HomeStep/index.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+  <main role="main">
+    <div class="content-wrap">
+      <article class="article">
+        <header class="header" role="banner">
+          <h1 class="page-header__heading">
+            <p>Walker Kennedy</p>
+          </h1>
+        </header>
+      </article>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      msg: 'i love panda'
+    }
+  }
+}
+</script>
